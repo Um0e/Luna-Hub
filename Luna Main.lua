16536fc25e2cf2f44aca2a2284ccf261dec5fd7b
LittleDyingDuck <51399074+LittleDyingDuck@users.noreply.github.com>
2020-07-31 23:21:37 +1000

    Changed to github loadstrings

--- Luna Main.lua
+++ Luna Main.lua
@@ -1,26 +1,26 @@
 --Project Jojo--
 if game.GameId == 798672140 then
-loadstring(game:HttpGet("https://pastebin.com/raw/ftdiSN9w", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/ProjectJojo.lua", true))()
 end
 --Lucky Battlegrounds--
 if game.GameId == 279565647 then
-loadstring(game:HttpGet("https://pastebin.com/raw/tJY32aMV", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/LuckyBlockBattlegrounds.lua", true))()
 end
 --Arsenal--
 if game.GameId == 111958650 then
-loadstring(game:HttpGet("https://pastebin.com/raw/dfdkra4p", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/Arsenal.lua", true))()
 end
 --Murder Mystery 2--
 if game.GameId == 66654135 then
-loadstring(game:HttpGet("https://pastebin.com/raw/6sSsdPKJ", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/MurderMystery2.lua", true))()
 end
 --Tower Of Hell--
 if game.GameId == 703124385 then
-loadstring(game:HttpGet("https://pastebin.com/raw/fNwSCvv5", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/TowerOfHell.lua", true))()
 end
 --A Bizarre Day (WIP)--
 if game.GameId == 969567031 then
-loadstring(game:HttpGet("https://pastebin.com/raw/70bGB9FV", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/ABizarreDay.lua", true))()
 end
 --Speedrun 4--
 if game.GameId == 83858907 then
@@ -28,5 +28,5 @@ loadstring(game:HttpGet("https://pastebin.com/raw/vqTThNRN", true))()
 end
 --Horrific Housing--
 if game.GameId == 107172930 then
-loadstring(game:HttpGet("https://pastebin.com/raw/8TaV7syN", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/HorifficHousing.lua", true))()
 end
