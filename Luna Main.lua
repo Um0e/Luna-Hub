c0dbead3999fb356e7a106dfd7f23f672656ea5f
Um0e <69041849+Um0e@users.noreply.github.com>
2020-08-01 18:26:46 +0100

    Update Luna Main.lua

--- Luna Main.lua
+++ Luna Main.lua
@@ -1,37 +1,37 @@
 --Project Jojo--
 if game.GameId == 798672140 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/ProjectJojo.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/ProjectJojo.lua", true))()
 end
 --Lucky Battlegrounds--
 if game.GameId == 279565647 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/LuckyBlockBattlegrounds.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/LuckyBlockBattlegrounds.lua", true))()
 end
 --Arsenal--
 if game.GameId == 111958650 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/Arsenal.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/Arsenal.lua", true))()
 end
 --Murder Mystery 2--
 if game.GameId == 66654135 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/MurderMystery2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/MurderMystery2.lua", true))()
 end
 --Tower Of Hell--
 if game.GameId == 703124385 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/TowerOfHell.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/TowerOfHell.lua", true))()
 end
 --A Bizarre Day (WIP)--
 if game.GameId == 969567031 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/ABizarreDay.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/ABizarreDay.lua", true))()
 end
 --Speedrun 4--
 if game.GameId == 83858907 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/SpeedRun4.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/SpeedRun4.lua", true))()
 end
 --Horrific Housing--
 if game.GameId == 107172930 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/HorifficHousing.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/HorifficHousing.lua", true))()
 end
 --No Scope Sniping: Remake (Extremely buggy, work in progress)--
 if game.GameId == 1760106570 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Luna-Hub/master/NoScopeSnipingRM.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/NoScopeSnipingRM.lua", true))()
 end
 
