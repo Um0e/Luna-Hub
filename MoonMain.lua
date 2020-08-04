ac550bcbf9f90b2f33cac79b612104a614d24414
Um0e <69041849+Um0e@users.noreply.github.com>
2020-08-04 12:20:09 +0100

    treacherous bruh added

--- MoonMain.lua
+++ MoonMain.lua
@@ -38,3 +38,7 @@ end
 if game.GameId == 1383164929 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Um0e/Moon-Hub/master/TreacherousTower.lua", true))()
 end
+--Prison Life--
+if game.GameId == numbers then
+loadstring(game:HttpGet(" ", true))()
+end
