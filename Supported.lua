9bbf71b0132287f8eaf6d3822d2436c34fc49165
Um0e <69041849+Um0e@users.noreply.github.com>
2020-08-04 12:42:15 +0100

    Supported Games File

---
+++ Supported.lua
@@ -0,0 +1,40 @@
+local tab1 = library:CreateTab("Supported Games", true)
+local button = library:MakeButton(tab1,"Project Jojo",function(button)
+    game:GetService("TeleportService"):Teleport(2295122555, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"A Bizarre Day",function(button)
+    game:GetService("TeleportService"):Teleport(2686500207, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Tower Of Hell",function(button)
+    game:GetService("TeleportService"):Teleport(1962086868, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Treacherous Tower",function(button)
+    game:GetService("TeleportService"):Teleport(4237861040, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Luckyblock Battle",function(button)
+    game:GetService("TeleportService"):Teleport(662417684, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Arsenal",function(button)
+    game:GetService("TeleportService"):Teleport(286090429, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Murder Mystery",function(button)
+    game:GetService("TeleportService"):Teleport(142823291, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Speedrun 4",function(button)
+    game:GetService("TeleportService"):Teleport(183364845, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"Horiffic Housing",function(button)
+    game:GetService("TeleportService"):Teleport(263761432, LocalPlayer)
+end)
+
+local button = library:MakeButton(tab1,"NoScope Sniping",function(button)
+    game:GetService("TeleportService"):Teleport(5081773298, LocalPlayer)
+end)
